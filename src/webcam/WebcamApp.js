d1a4d09781ef78d91d1d2afaffe58f9c81e808cf
gc734207 <swr.iku.nagae@gmail.com>
2024-12-04 18:18:46 -0800

    Made clicking the "New Capture" button overwrite the previous video. Avoids duplicates videos and download buttons appearing.

--- src/webcam/WebcamApp.js
+++ src/webcam/WebcamApp.js
@@ -5,8 +5,10 @@ import './WebcamApp.css';
 const WebcamApp = () => {
   const webcamRef = React.useRef(null);
   const mediaRecorderRef = React.useRef(null);
+  const videoToolsArea = React.useRef(null);
   const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
+
 
   const handleDataAvailable = React.useCallback(
     ({ data }) => {
@@ -18,6 +20,7 @@ const WebcamApp = () => {
   );
 
   const handleStartCaptureClick = React.useCallback(() => {
+    setRecordedChunks([]);
     setCapturing(true);
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
       mimeType: "video/webm"
@@ -35,47 +38,51 @@ const WebcamApp = () => {
   }, [mediaRecorderRef, setCapturing]);
 
   React.useEffect(() => {
-    if (recordedChunks.length) {
+    if (recordedChunks.length > 0) {
+      if (videoToolsArea.current) {
+        videoToolsArea.current.innerHTML = '';
+        videoToolsArea.current.remove();
+      }
+
       const blob = new Blob(recordedChunks, {
         type: "video/webm"
       });
       const url = URL.createObjectURL(blob);
+      const div = document.createElement("div");
+
+      const a = document.createElement("a");
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+
+      const button = document.createElement("button");
+      button.onclick = function() {
+        a.click();
+      }
+      button.textContent = "Download";
+
       const video = document.createElement("video");
       const source = document.createElement("source");
       video.setAttribute("controls","controls");
       source.src = url;
       video.appendChild(source);
-      document.body.appendChild(video);
-    }
-  }, [recordedChunks]);
+      video.appendChild(source);
 
-  const handleDownload = React.useCallback(() => {
-    if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm"
-      });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
-      a.click();
+      document.body.appendChild(div);
+      div.appendChild(a);
+      div.appendChild(button);
+      div.appendChild(video);
+      videoToolsArea.current = div;
     }
   }, [recordedChunks]);
 
   return (
     <>
       <Webcam audio={false} ref={webcamRef} />
-      {capturing ? (
-        <button onClick={handleStopCaptureClick}>Stop Capture</button>
-      ) : (
-        <button onClick={handleStartCaptureClick}>Start Capture</button>
-      )}
-
-      {recordedChunks.length > 0 && (
-        <button onClick={handleDownload}>Download</button>
-      )}
+      {capturing 
+        ? <button onClick={handleStopCaptureClick}>Stop Capture</button>
+        : <button onClick={handleStartCaptureClick}>New Capture</button>
+      }
     </>
   );
 };
